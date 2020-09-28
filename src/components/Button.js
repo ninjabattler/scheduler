1878e0d94a05243bca91c09ae0776b0b96856ece
Jayden Tucker <jaydenrtucker@gmail.com>
2020-09-28 14:05:54 -0700

    Refactored the button template to use classnames

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,13 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from 'classnames';
+
 export default function Button(props) {
-   let buttonClass = "button";
-
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-
-  if (props.danger) {
-   buttonClass += " button--danger";
-  }
-
-  return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
+   return <button
+   className={classNames('button', {'button--confirm': props.confirm}, {'button--danger': props.danger})}
+   onClick={props.onClick}
+   disabled={props.disabled}>
+      {props.children}
+   </button>;
 }
