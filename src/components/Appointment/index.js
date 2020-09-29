52a51950ce382c136add5c616b441482e0d20989
Jayden Tucker <jaydenrtucker@gmail.com>
2020-09-29 16:40:38 -0700

    The index.js file now properly displays appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import 'components/Appointment/styles.scss';
+import Header from 'components/Appointment/header';
+import Show from 'components/Appointment/show';
+import Empty from 'components/Appointment/empty';
+
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -135,8 +135,22 @@ storiesOf("Appointment", module)
   .addParameters({
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
-  .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time='12pm' />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer: interviewers[0] }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
   .add("Header", () => <Header time='12pm' />)
   .add("Empty", () => <Empty onClick={action("onAdd")} />)
   .add("Show", () => <Show student='Lydia Miller-Jones' interviewer={interviewers[1]} onEdit={action('edit')} onDelete={action('delete')}/>)
