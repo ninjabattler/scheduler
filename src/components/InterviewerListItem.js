47fb51931053f0faf6ef9800763a455685e5a21b
Jayden Tucker <jaydenrtucker@gmail.com>
2020-09-29 12:44:29 -0700

    Added the InterviewerListItem template and added it to storybook

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import 'components/InterviewerListItem.scss';
+import classNames from 'classnames';
+
+export default function InterviewerListItem(props) {
+
+  const className = classNames('interviewers__item', {'interviewers__item--selected': props.selected},{'interviewers__item--image': props.avatar})
+  let name;
+  props.selected ? name = props.name : name = '';
+  let onClick;
+  props.setInterviewer ? onClick = ()=>{props.setInterviewer(props.name)} : onClick = ()=>{};
+
+  return (
+    <li className={className} onClick={onClick}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt="Sylvia Palmer"
+      />
+      {name}
+    </li>
+  );
+}
