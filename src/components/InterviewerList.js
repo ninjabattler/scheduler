a8b0b9ee8519d2bc93e92d517d0f08e6648e1e55
Jayden Tucker <jaydenrtucker@gmail.com>
2020-10-05 12:57:20 -0700

    Added PropTypes to the interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import 'components/InterviewerList.scss';
 import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
@@ -19,3 +20,8 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
